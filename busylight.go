519c893dee94ecd8e9adeb1efdd06e799cfa370a
BaaaZen <baaazen@gmail.com>
2019-04-01 15:53:25 +0200

    Simple support (only color) for Kuando Busylight

--- busylight.go
+++ busylight.go
@@ -1,9 +1,10 @@
 package led
 
 import (
-	"github.com/baaazen/go-hid"
 	"image/color"
 	"time"
+
+	"github.com/baaazen/go-hid"
 )
 
 // Device type: BusyLight UC
@@ -11,6 +12,9 @@ var BusyLightUC DeviceType
 
 // Device type: BusyLight Lync
 var BusyLightLync DeviceType
+
+// Device type: Kuando BusyLight
+var KuandoBusyLight DeviceType
 
 func init() {
 	BusyLightUC = addDriver(usbDriver{
@@ -34,6 +38,57 @@ func init() {
 			return newBusyLight(d, func(c color.Color) {
 				r, g, b, _ := c.RGBA()
 				d.Write([]byte{0x00, 0x00, 0x00, byte(r >> 8), byte(g >> 8), byte(b >> 8), 0x00, 0x00, 0x00})
+			}), nil
+		},
+	})
+	KuandoBusyLight = addDriver(usbDriver{
+		/*
+			Protocol:
+			===================================================================
+			[1 byte ] 0x00				(always)
+			------------ step -------------------------------------------------
+			[1 byte ] 0x10				next step id (always OR 0x10)
+			[1 byte ] 0x01				repeat interval
+			[3 bytes] 0xFF 0x00 0x00	R/G/B each 8 bit unsigned
+			[1 byte ] 0x00				"on" timing
+			[1 byte ] 0x00				"off" timing
+			[1 byte ] 0x80				sound id and volume
+										=> 0x80 + sound_id * 0x3 + sound_volume
+			------------ (repeat) ---------------------------------------------
+			=> fill up with 0x00 until len(steps) >= 56
+			------------ footer -----------------------------------------------
+			[6 bytes] 0x06 0x04 0x55 0xff 0xff 0xff   (always)
+			[2 bytes] 0x.. 0x..         checksum: 16 bit unsigned int
+										=> sum of all bytes
+			===================================================================
+		*/
+		Name:      "Kuando BusyLight",
+		Type:      &KuandoBusyLight,
+		VendorId:  0x27BB,
+		ProductId: 0x3BCD,
+		Open: func(d hid.Device) (Device, error) {
+			return newBusyLight(d, func(c color.Color) {
+				r, g, b, _ := c.RGBA()
+
+				steps := []byte{0x10, 0x01, byte(r >> 8), byte(g >> 8), byte(b >> 8), 0x01, 0x00, 0x80}
+				for len(steps) < 56 {
+					steps = append(steps, 0x00)
+				}
+
+				// header and footer
+				buffer := []byte{0x00}
+				buffer = append(buffer, steps...)
+				buffer = append(buffer, 0x06, 0x04, 0x55, 0xff, 0xff, 0xff)
+
+				// calculate checksum
+				var checksum uint16 = 0
+				for _, bufferByte := range buffer {
+					checksum += uint16(bufferByte & 0xff)
+				}
+				buffer = append(buffer, byte(checksum>>8), byte(checksum&0xff))
+
+				// send buffer
+				d.Write(buffer)
 			}), nil
 		},
 	})

18b2197b86bf677a6fe805a80492b5cc916d7ebe
BaaaZen <baaazen@gmail.com>
2019-04-15 16:38:21 +0200

    Structs for sound and animation

---
+++ animation.go
@@ -0,0 +1,69 @@
+package led
+
+import (
+	"image/color"
+)
+
+type ledAnimationFrame struct {
+	prevFrame *ledAnimationFrame
+	nextFrame *ledAnimationFrame
+	color color.Color
+	onTiming uint8
+	offTiming uint8
+	repeatInterval uint8
+	sound *ledSound
+}
+
+func newLedAnimation() *ledAnimationFrame {
+	return &ledAnimationFrame{}
+}
+
+func (f *ledAnimationFrame) FirstFrame() *ledAnimationFrame {
+	if f.prevFrame == nil {
+		return f
+	} else {
+		return f.prevFrame.FirstFrame()
+	}
+}
+
+func (f *ledAnimationFrame) PrevFrame() *ledAnimationFrame {
+	return f.prevFrame
+}
+
+func (f *ledAnimationFrame) NextFrame() *ledAnimationFrame {
+	return f.nextFrame
+}
+
+func (f *ledAnimationFrame) AppendFrame() *ledAnimationFrame {
+	f.nextFrame = &ledAnimationFrame{prevFrame: f}
+	return f.nextFrame
+}
+
+func (f *ledAnimationFrame) GetID() uint8 {
+	if f.prevFrame == nil {
+		return 0
+	} else {
+		return f.prevFrame.GetID() + 1
+	}
+}
+
+func (f *ledAnimationFrame) SetNextFrame(nextFrame *ledAnimationFrame) {
+	f.nextFrame = nextFrame
+}
+
+func (f *ledAnimationFrame) SetColor(c color.Color) {
+	f.color = c
+}
+
+func (f *ledAnimationFrame) SetTiming(onTiming, offTiming uint8) {
+	f.onTiming = onTiming
+	f.offTiming = offTiming
+}
+
+func (f *ledAnimationFrame) SetRepeatInterval(repeatInterval uint8) {
+	f.repeatInterval = repeatInterval
+}
+
+func (f *ledAnimationFrame) SetSound(sound *ledSound) {
+	f.sound = sound
+}
---
+++ sound.go
@@ -0,0 +1,15 @@
+package led
+
+type ledSound struct {
+	soundID uint8
+	volume uint8
+}
+
+func newLedSound() *ledSound {
+	return &ledSound{}
+}
+
+func (s *ledSound) SetSound(soundID, volume uint8) {
+	s.soundID = soundID
+	s.volume = volume
+}
